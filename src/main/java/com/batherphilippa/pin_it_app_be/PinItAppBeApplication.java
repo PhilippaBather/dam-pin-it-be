e5103dae60d0a3a73003ef6c077150ee8b163217
pabather <p.a.bather1@gmail.com>
2024-10-14 22:04:45 +0200

    added entry point

---
+++ src/main/java/com/batherphilippa/pin_it_app_be/PinItAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.batherphilippa.pin_it_app_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinItAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinItAppBeApplication.class, args);
+	}
+
+}
