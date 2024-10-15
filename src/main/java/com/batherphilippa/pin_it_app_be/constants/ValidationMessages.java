ca85150a09e80a666dfaba53032369330aa0262b
pabather <p.a.bather1@gmail.com>
2024-10-15 22:46:33 +0200

    added constants

---
+++ src/main/java/com/batherphilippa/pin_it_app_be/constants/ValidationMessages.java
@@ -0,0 +1,21 @@
+package com.batherphilippa.pin_it_app_be.constants;
+
+public class ValidationMessages {
+    public static final String VALIDATION_COLLABORATOR_EMAIL = "An collaborator's email is required.";
+    public static final String VALIDATION_COLLABORATOR_ROLE = "A role must be assigned to a collaborator";
+    public static final String VALIDATION_EMAIL_NOT_BLANK = "An user's email is required.";
+    public static final String VALIDATION_FORENAME_NOT_BLANK = "A name is required.";
+    public static final String VALIDATION_FORENAME_SIZE = "A name must be between 2 and 50 characters inclusive.";
+    public static final String VALIDATION_PASSWORD_NOT_BLANK = "A password is required";
+    public static final String VALIDATION_PASSWORD_REGEX_CONSTRAINTS = "Password must contain minimum 8 characters, including at least 1 digit and 1 special character; the maximum length is of 25 characters";
+    public static final String VALIDATION_PROJECT_DEADLINE = "A project deadline is required.";
+    public static final String VALIDATION_PROJECT_TITLE_NOT_BLANK = "A title is required.";
+    public static final String VALIDATION_PROJECT_TITLE_SIZE = "A title must be between 2 and 50 characters inclusive.";
+    public static final String VALIDATION_SURNAME_NOT_BLANK = "A surname must be entered.";
+    public static final String VALIDATION_SURNAME_SIZE = "A surname must be between 2 and 100 characters inclusive.";
+    public static final String VALIDATION_TASK_DESCRIPTION_NOT_BLANK = "A description is required.";
+    public static final String VALIDATION_TASK_TITLE_NOT_BLANK = "A task title is required.";
+    public static final String VALIDATION_TASK_TITLE_SIZE = "A task title must be between 2 and 50  characters inclusive.";
+    public static final String VALIDATION_USERNAME_NOT_BLANK = "A username is required.";
+    public static final String VALIDATION_USERNAME_SIZE = "A username must be between 2 and 15 characters inclusive.";
+}
---
+++ src/main/java/com/batherphilippa/pin_it_app_be/constants/ValidationRegex.java
@@ -0,0 +1,6 @@
+package com.batherphilippa.pin_it_app_be.constants;
+
+public class ValidationRegex {
+
+    public static final String VALIDATION_PASSWORD_REGEX = "^(?=.*[\\d])(?=.*[!@#$%^&*_])[\\w!@#$%^&*]{8,25}$";
+}
