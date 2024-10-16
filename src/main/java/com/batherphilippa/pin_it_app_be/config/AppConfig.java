9b19a7167dfd658bcbba72ebb29aff3986901982
pabather <p.a.bather1@gmail.com>
2024-10-16 22:54:35 +0200

    added comments

--- src/main/java/com/batherphilippa/pin_it_app_be/config/AppConfig.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/config/AppConfig.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * AppConfig - application configuration.
+ */
 @Configuration
 public class AppConfig {
 
