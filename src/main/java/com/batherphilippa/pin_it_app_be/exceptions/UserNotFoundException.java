b49a9129b67e6e93d5042a77a76a762d0d31d269
pabather <p.a.bather1@gmail.com>
2024-10-16 22:55:15 +0200

    added comments

--- src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserNotFoundException.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserNotFoundException.java
@@ -2,6 +2,9 @@ package com.batherphilippa.pin_it_app_be.exceptions;
 
 import static com.batherphilippa.pin_it_app_be.constants.ErrorMessages.EXCEPTION_USER_NOT_FOUND;
 
+/**
+ * UserNotFoundException - exception thrown when registered user not found by ID.
+ */
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(long userId) {
