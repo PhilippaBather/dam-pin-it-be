f28c7957aedca116daa65f59ea50629dfc923074
pabather <p.a.bather1@gmail.com>
2024-10-27 14:47:14 +0100

    updated for auth login and signup

--- src/main/java/com/batherphilippa/pin_it_app_be/service/IUserService.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/service/IUserService.java
@@ -2,6 +2,7 @@ package com.batherphilippa.pin_it_app_be.service;
 
 import com.batherphilippa.pin_it_app_be.dto.UserDTOIn;
 import com.batherphilippa.pin_it_app_be.dto.UserDTOOut;
+import com.batherphilippa.pin_it_app_be.dto.UserLoginDTOIn;
 
 import java.util.Set;
 
@@ -12,7 +13,9 @@ public interface IUserService {
 
     Set<UserDTOOut> findAll();
     UserDTOOut save(UserDTOIn userDTOIn);
+    boolean findByEmail(String email);
     UserDTOOut findById(long userId);
+    UserDTOOut findUserOnLogin(UserLoginDTOIn user);
     UserDTOOut updateById(long userId, UserDTOIn userDTOIn);
     void deleteById(long userId);
 }
