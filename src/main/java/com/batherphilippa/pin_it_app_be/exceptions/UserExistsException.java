d0f50e697a14c03a520470df5a7623371eca1b35
pabather <p.a.bather1@gmail.com>
2024-10-27 14:41:26 +0100

    updated

--- src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserExistsException.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserExistsException.java
@@ -2,6 +2,9 @@ package com.batherphilippa.pin_it_app_be.exceptions;
 
 import static com.batherphilippa.pin_it_app_be.constants.ErrorMessages.EXCEPTION_USER_EXISTS;
 
+/**
+ * UserExistsException - exception thrown when a conflict of email addresses occurs on signup.
+ */
 public class UserExistsException extends RuntimeException {
 
     public UserExistsException(String email) {
--- src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserNotFoundException.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/exceptions/UserNotFoundException.java
@@ -1,13 +1,17 @@
 package com.batherphilippa.pin_it_app_be.exceptions;
 
-import static com.batherphilippa.pin_it_app_be.constants.ErrorMessages.EXCEPTION_USER_NOT_FOUND;
+import static com.batherphilippa.pin_it_app_be.constants.ErrorMessages.*;
 
 /**
- * UserNotFoundException - exception thrown when registered user not found by ID.
+ * UserNotFoundException - exception thrown when registered user not found.
  */
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(long userId) {
-        super(EXCEPTION_USER_NOT_FOUND + userId);
+        super(EXCEPTION_USER_NOT_FOUND_ID + userId);
+    }
+
+    public UserNotFoundException(String email) {
+        super(EXCEPTION_USER_NOT_FOUND_EMAIL + email);
     }
 }
