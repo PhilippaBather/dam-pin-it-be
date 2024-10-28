50f85612bf7edf038ddc8581654d29a79b3cb145
pabather <p.a.bather1@gmail.com>
2024-10-28 20:43:35 +0100

    removed regex check as now jwt

--- src/main/java/com/batherphilippa/pin_it_app_be/model/User.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/model/User.java
@@ -3,7 +3,6 @@ package com.batherphilippa.pin_it_app_be.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +12,6 @@ import java.util.Iterator;
 import java.util.Set;
 
 import static com.batherphilippa.pin_it_app_be.constants.ValidationMessages.*;
-import static com.batherphilippa.pin_it_app_be.constants.ValidationRegex.VALIDATION_PASSWORD_REGEX;
 
 /**
  * User - defines a registered user of the application.
@@ -45,9 +43,6 @@ public class User {
     @Column(name = "email")
     String email;
 
-    // password must include at least one character, one uppercase character, and one special character
-    // password must be between 8 and 25 characters inclusive
-    @Pattern(regexp = VALIDATION_PASSWORD_REGEX, message = VALIDATION_PASSWORD_REGEX_CONSTRAINTS)
     @NotBlank(message = VALIDATION_PASSWORD_NOT_BLANK)
     @Column(name = "password")
     private String password;
