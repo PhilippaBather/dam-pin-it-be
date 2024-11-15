d22eac9b1003b661b9d9b8068f42ea792ad1afde
pabather <p.a.bather1@gmail.com>
2024-11-15 21:03:49 +0100

    udpated

--- src/main/java/com/batherphilippa/pin_it_app_be/service/IUserService.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/service/IUserService.java
@@ -2,6 +2,7 @@ package com.batherphilippa.pin_it_app_be.service;
 
 import com.batherphilippa.pin_it_app_be.dto.UserDTOIn;
 import com.batherphilippa.pin_it_app_be.dto.UserDTOOut;
+import com.batherphilippa.pin_it_app_be.exceptions.UserNotFoundException;
 import com.batherphilippa.pin_it_app_be.model.User;
 
 import java.util.Set;
@@ -12,7 +13,8 @@ import java.util.Set;
 public interface IUserService {
 
     Set<UserDTOOut> findAll();
-    User findById(long userId);
-    UserDTOOut updateById(long userId, UserDTOIn userDTOIn);
-    void deleteById(long userId);
+    User findById(long userId) throws UserNotFoundException;
+    UserDTOOut findByEmail(String email) throws UserNotFoundException;
+    UserDTOOut updateById(long userId, UserDTOIn userDTOIn) throws UserNotFoundException;
+    void deleteById(long userId) throws UserNotFoundException;
 }
