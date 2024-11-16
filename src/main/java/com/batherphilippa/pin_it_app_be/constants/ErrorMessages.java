9ddf9a281def1f31b23d652692e576f0bfb07c1f
pabather <p.a.bather1@gmail.com>
2024-11-16 22:18:48 +0100

    updated err and validtion msgs for tasks

--- src/main/java/com/batherphilippa/pin_it_app_be/constants/ErrorMessages.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/constants/ErrorMessages.java
@@ -6,6 +6,7 @@ package com.batherphilippa.pin_it_app_be.constants;
 public class ErrorMessages {
 
     public static final String EXCEPTION_PROJECT_NOT_FOUND_ID = "Project not found with ID ";
+    public static final String EXCEPTION_TASK_NOT_FOUND_ID = "Task not found with ID ";
     public static final String EXCEPTION_USER_EXISTS = "User already exists with email ";
     public static final String EXCEPTION_USER_NOT_FOUND_ID = "User not found with ID ";
     public static final String EXCEPTION_USER_NOT_FOUND_EMAIL = "User not found with email address ";
