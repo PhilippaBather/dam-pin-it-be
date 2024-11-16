2cb699a7f05817595d5aba36184a5decd2962df3
pabather <p.a.bather1@gmail.com>
2024-11-16 22:20:06 +0100

    added task repp

---
+++ src/main/java/com/batherphilippa/pin_it_app_be/repository/TaskRepo.java
@@ -0,0 +1,25 @@
+package com.batherphilippa.pin_it_app_be.repository;
+
+import com.batherphilippa.pin_it_app_be.dto.TaskDetailsDTOOut;
+import com.batherphilippa.pin_it_app_be.model.Task;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface TaskRepo extends CrudRepository<Task, Long> {
+
+    @Query(value = "SELECT t.task_id, t.task_deadline, t.priority_level, t.title FROM tasks t " +
+            " WHERE project_id = :projectId;", nativeQuery = true)
+    Set<TaskDetailsDTOOut> findAllTasksByProjectId(long projectId);
+
+    Optional<Task> getTaskById(long taskId);
+
+    @Query(value = "DELETE * FROM tasks t " +
+            " WHERE project_id =:projectId;", nativeQuery = true)
+    void deleteAllByProjectId(long projectId);
+
+}
