3cba77ed1323d4b1d1cf4c1f038dda77ac3df6c7
pabather <p.a.bather1@gmail.com>
2024-11-17 11:25:14 +0100

    updated and added automatic flushing for modifying update transactions

--- src/main/java/com/batherphilippa/pin_it_app_be/repository/TaskRepo.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/repository/TaskRepo.java
@@ -1,11 +1,15 @@
 package com.batherphilippa.pin_it_app_be.repository;
 
-import com.batherphilippa.pin_it_app_be.dto.TaskDetailsDTOOut;
+import com.batherphilippa.pin_it_app_be.model.Priority;
 import com.batherphilippa.pin_it_app_be.model.Task;
+import com.batherphilippa.pin_it_app_be.model.TaskStatus;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
 
@@ -15,14 +19,25 @@ import java.util.Set;
 @Repository
 public interface TaskRepo extends CrudRepository<Task, Long> {
 
-    @Query(value = "SELECT t.task_id, t.task_deadline, t.priority_level, t.title FROM tasks t " +
-            " WHERE project_id = :projectId;", nativeQuery = true)
-    Set<TaskDetailsDTOOut> findAllTasksByProjectId(long projectId);
+    @Query(value = "SELECT t.task_id, t.task_deadline, t.task_description, t.priority_level, t.task_title, t.project_id, " +
+            " t.task_position, t.task_status FROM tasks t " +
+            " WHERE t.project_id = :projectId;", nativeQuery = true)
+    Set<Task> findAllTasksByProjectId(long projectId);
 
     Optional<Task> getTaskById(long taskId);
 
-    @Query(value = "DELETE * FROM tasks t " +
-            " WHERE project_id =:projectId;", nativeQuery = true)
+    @Transactional
+    @Modifying(clearAutomatically=true) // clears non-flushed values from the EntityManager
+    @Query(value = "UPDATE tasks t" +
+            " SET t.task_deadline =:deadline, t.task_description = :description, t.priority_level = :priorityLevel," +
+            " t.task_title = :title, t.task_position = :taskPosition, t.task_status = :taskStatus " +
+            " WHERE t.task_id = :taskId;", nativeQuery = true)
+    void updateTaskById(long taskId, LocalDate deadline, String description, Priority priorityLevel, String title, int taskPosition, TaskStatus taskStatus);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM tasks t " +
+            " WHERE t.project_id =:projectId;", nativeQuery = true)
     void deleteAllByProjectId(long projectId);
 
 }
