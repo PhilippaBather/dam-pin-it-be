448a546db922f4bed63050d0bf7d578097712e44
pabather <p.a.bather1@gmail.com>
2024-11-21 22:48:26 +0100

    updated email service; added guest service

---
+++ src/main/java/com/batherphilippa/pin_it_app_be/service/GuestService.java
@@ -0,0 +1,11 @@
+package com.batherphilippa.pin_it_app_be.service;
+
+import com.batherphilippa.pin_it_app_be.model.Guest;
+
+public class GuestService implements IGuestService {
+
+    @Override
+    public Guest saveGuest(Guest guest) {
+        return null;
+    }
+}
---
+++ src/main/java/com/batherphilippa/pin_it_app_be/service/IGuestService.java
@@ -0,0 +1,8 @@
+package com.batherphilippa.pin_it_app_be.service;
+
+import com.batherphilippa.pin_it_app_be.model.Guest;
+
+public interface IGuestService {
+
+    Guest saveGuest(Guest guest);
+}
