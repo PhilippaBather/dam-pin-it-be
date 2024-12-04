981a63ac1f09c96249ce72b8733e8d2ca65384c5
pabather <p.a.bather1@gmail.com>
2024-12-04 20:30:41 +0100

    updated secured routes

--- src/main/java/com/batherphilippa/pin_it_app_be/config/AppConfig.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/config/AppConfig.java
@@ -104,7 +104,7 @@ public class AppConfig {
                                         "/project/*", "project/user/*/project/*", "/tasks/user/*/project/*",
                                         "/tasks-list/user/*/project/*", "task/*/user/*/project/*",
                                         "/guests", "/guests/shared-projects/*", "/guests/*/shared-projects/*",
-                                        "/guests/owned-projects/*").permitAll()
+                                        "/guests/owned-projects/*", "/guests/owned-projects/guest/*/project/*").permitAll()
                                 .anyRequest().authenticated()
                 );
 
