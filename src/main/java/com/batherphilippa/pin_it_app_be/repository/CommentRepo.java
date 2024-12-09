05da2a4103ab55250b33cdf5c954d51a1b4e75a3
pabather <p.a.bather1@gmail.com>
2024-12-09 04:27:41 +0100

    added comment repo

--- src/main/java/com/batherphilippa/pin_it_app_be/repository/CommentRepo.java
+++ src/main/java/com/batherphilippa/pin_it_app_be/repository/CommentRepo.java
@@ -1,2 +1,16 @@
-package com.batherphilippa.pin_it_app_be.repository;public class CommentRepo {
+package com.batherphilippa.pin_it_app_be.repository;
+
+import com.batherphilippa.pin_it_app_be.model.TaskComment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * CommentRepo - the Comment Repository containing additional helper methods.
+ */
+@Repository
+public interface CommentRepo extends CrudRepository<TaskComment, Long> {
+
+    List<TaskComment> findByTaskId(long taskId);
 }
